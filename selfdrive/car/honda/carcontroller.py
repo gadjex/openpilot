e1fdc6a658eeb22b457b824f927670dc8fa9163c
Rob Chouinard <gadjex@gmail.com>
2023-01-15 17:00:51 -0500

    Rename function on hud set speed fix

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -106,7 +106,7 @@ def rate_limit_steer(new_steer, last_steer):
   MAX_DELTA = 3 * DT_CTRL
   return clip(new_steer, last_steer - MAX_DELTA, last_steer + MAX_DELTA)
 
-def calculate_hud_max_speed(hud_v_cruise_kph, is_metric):
+def calculate_hud_set_speed(hud_v_cruise_kph, is_metric):
   if (is_metric):
     return hud_v_cruise_kph
   cruise_mph = round(hud_v_cruise_kph * CV.KPH_TO_MPH)
@@ -253,7 +253,7 @@ class CarController:
 
     # Send dashboard UI commands.
     if self.frame % 10 == 0:
-      hud_set_speed = calculate_hud_max_speed(hud_v_cruise, CS.is_metric)
+      hud_set_speed = calculate_hud_set_speed(hud_v_cruise, CS.is_metric)
       hud = HUDData(int(pcm_accel), hud_set_speed, hud_control.leadVisible,
                     hud_control.lanesVisible, fcw_display, acc_alert, steer_required)
       can_sends.extend(hondacan.create_ui_commands(self.packer, self.CP, CC.enabled, pcm_speed, hud, CS.is_metric, CS.acc_hud, CS.lkas_hud))
